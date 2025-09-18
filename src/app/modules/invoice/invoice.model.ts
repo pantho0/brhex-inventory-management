19cdad3b03ff50c461c67d1e137bd7419094054c
Mashraki Toki Tahmid <panthomashreky6@gmail.com>
2025-09-18 14:05:01 +0600

    added invoice model

---
+++ src/app/modules/invoice/invoice.model.ts
@@ -0,0 +1,69 @@
+import { model, Schema } from 'mongoose';
+import { IInvoice } from './invoice.interface';
+
+const invoiceSchema = new Schema<IInvoice>(
+  {
+    invoiceNo: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    customerName: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+    },
+    mobile: {
+      type: String,
+    },
+    items: [
+      {
+        productId: { type: Schema.Types.ObjectId, ref: 'Product' },
+        productName: String,
+        serialNumber: String,
+        price: Number,
+      },
+    ],
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    discount: {
+      type: Number,
+    },
+    tax: {
+      type: Number,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    paidAmount: {
+      type: Number,
+      default: 0,
+    },
+    dueAmount: {
+      type: Number,
+      default: 0,
+    },
+    paymentStatus: {
+      type: String,
+      enum: ['paid', 'partial', 'due'],
+      default: 'due',
+    },
+    paymentHistory: [
+      {
+        date: Date,
+        amount: Number,
+        method: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Invoice = model<IInvoice>('Invoice', invoiceSchema);
