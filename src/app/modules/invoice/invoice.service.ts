30125d82c99a58cbc43a903e6f3115ed96085a94
Mashraki Toki Tahmid <panthomashreky6@gmail.com>
2025-09-20 12:00:29 +0600

    added inventory creating service api

--- src/app/modules/invoice/invoice.service.ts
+++ src/app/modules/invoice/invoice.service.ts
@@ -11,29 +11,43 @@ const createInvoiceIntoDB = async (invoiceData: IInvoice) => {
     const {
       invoiceNo,
       customerName,
+      mobile,
+      address,
       items,
-      subtotal,
-      discount,
-      tax,
-      total,
+      discount = 0,
+      tax = 0,
+      paidAmount = 0,
       paymentStatus,
-      paidAmount,
     } = invoiceData;
 
-    let dueAmount = total - (paidAmount || 0);
+    const subtotal = items.reduce((acc, item) => acc + item.price, 0);
+
+    const discountedAmount = subtotal - discount;
+
+    const taxAmount = (discountedAmount * tax) / 100;
+
+    const total = discountedAmount + taxAmount;
+
+    const dueAmount = total - paidAmount;
+
+    const finalStatus =
+      paymentStatus ||
+      (dueAmount === 0 ? 'paid' : paidAmount > 0 ? 'partial' : 'due');
 
     const invoice = await Invoice.create(
       [
         {
           invoiceNo,
           customerName,
+          mobile,
+          address,
           items,
           subtotal,
           discount,
           tax,
           total,
-          paymentStatus: paymentStatus || (dueAmount === 0 ? 'paid' : 'due'),
-          paidAmount: paidAmount || 0,
+          paymentStatus: finalStatus,
+          paidAmount,
           dueAmount,
           paymentHistory: paidAmount
             ? [{ amount: paidAmount, method: 'cash', date: new Date() }]
