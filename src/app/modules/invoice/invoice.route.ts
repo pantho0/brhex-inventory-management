c1deba00744855209771257de470625d69238b69
Mashraki Toki Tahmid <panthomashreky6@gmail.com>
2025-09-28 16:43:32 +0600

    fixed the routing issue of sales summary

--- src/app/modules/invoice/invoice.route.ts
+++ src/app/modules/invoice/invoice.route.ts
@@ -4,9 +4,9 @@ import { InvoiceController } from './invoice.controller';
 const router = Router();
 
 router.post('/create-invoice', InvoiceController.createInvoice);
+router.post('/sales-summary', InvoiceController.getMultiPeriodSalesSummary);
 router.get('/', InvoiceController.getAllInvoices);
 router.get('/:id', InvoiceController.getSingleInvoice);
 router.put('/payment/:id', InvoiceController.updatePayment);
-router.get('/sales-summary', InvoiceController.getMultiPeriodSalesSummary);
 
 export const InvoiceRoute = router;
