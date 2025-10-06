63f455a012447717647b0f8d8c80859d81b17395
Mashraki Toki Tahmid <panthomashreky6@gmail.com>
2025-10-06 13:01:17 +0600

    auth guard added to usr management

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,14 +1,24 @@
 import { Router } from 'express';
 import { UserControllers } from './user.controller';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.const';
 
 const router = Router();
 
-router.get('/', UserControllers.getAllUser);
-router.get('/:id', UserControllers.getSingleUser);
-router.get('/me', UserControllers.getMe);
-router.post('/create-user', UserControllers.createUser);
-router.put('/change-role', UserControllers.changeUserRole);
-router.put('/block-user', UserControllers.blockUser);
-router.put('/delete-user', UserControllers.deleteUser);
+router.get('/', auth(USER_ROLE.admin), UserControllers.getAllUser);
+router.get('/me', auth(USER_ROLE.admin, USER_ROLE.user), UserControllers.getMe);
+router.post('/create-user', auth(USER_ROLE.admin), UserControllers.createUser);
+router.put(
+  '/change-role',
+  auth(USER_ROLE.admin),
+  UserControllers.changeUserRole,
+);
+router.put('/block-user', auth(USER_ROLE.admin), UserControllers.blockUser);
+router.put('/delete-user', auth(USER_ROLE.admin), UserControllers.deleteUser);
+router.get(
+  '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.user),
+  UserControllers.getSingleUser,
+);
 
 export const UserRoutes = router;
