5376e0e67494418c704ac1ade94b354f18b884fe
Mashraki Toki Tahmid <panthomashreky6@gmail.com>
2025-10-06 12:07:48 +0600

    added validation and auth middleware and removed from user

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,26 +1,22 @@
 import { Router } from 'express';
 import { UserControllers } from './user.controller';
-import validateRequest from '../../middlewares/validateRequest';
-import { createUserValidationSchema } from './user.validation';
-import { USER_ROLE } from './user.const';
-import auth from '../../middlewares/auth';
 
 const router = Router();
 
-router.get('/', auth(USER_ROLE.admin), UserControllers.getAllUser);
-router.get('/:id', auth(USER_ROLE.admin), UserControllers.getSingleUser);
-router.get('/me', auth(USER_ROLE.admin, USER_ROLE.user), UserControllers.getMe);
+router.get('/', UserControllers.getAllUser);
+router.get('/:id', UserControllers.getSingleUser);
+router.get('/me', UserControllers.getMe);
 router.post(
   '/create-user',
-  validateRequest(createUserValidationSchema),
+
   UserControllers.createUser,
 );
 router.put(
   '/change-role',
-  auth(USER_ROLE.admin),
+
   UserControllers.changeUserRole,
 );
-router.put('/block-user', auth(USER_ROLE.admin), UserControllers.blockUser);
-router.put('/delete-user', auth(USER_ROLE.admin), UserControllers.deleteUser);
+router.put('/block-user', UserControllers.blockUser);
+router.put('/delete-user', UserControllers.deleteUser);
 
 export const UserRoutes = router;
